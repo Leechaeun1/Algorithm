447f071723a6c8f8485fd04ee99270eeefb04263
채은 <88484000+Leechaeun1@users.noreply.github.com>
2025-04-28 18:11:30 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,13 +1,11 @@
 n = int(input())
-li = [0,1] + [0]*(n - 1) #리스트를 크기만큼 초기화
+
 def fibonacci(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
-    else:
-        for i in range(2, n+1):
-            li[i] = li[i - 1] + li[i - 2]
-        return li[n]
+    elif n >= 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 print(fibonacci(n))
