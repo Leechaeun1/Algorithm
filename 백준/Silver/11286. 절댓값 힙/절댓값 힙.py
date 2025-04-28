22038b291d1d1915ecc946abc0ee9615ebf69a54
채은 <88484000+Leechaeun1@users.noreply.github.com>
2025-04-28 15:35:32 +0900

    [Silver I] Title: 절댓값 힙, Time: 256 ms, Memory: 45748 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -5,12 +5,12 @@ n = int(sys.stdin.readline())
 q = PriorityQueue()
 
 for i in range(n):
-    request = int(sys.stdin.readline())
-    if request == 0:
+    x = int(sys.stdin.readline())
+    if x == 0:
         if q.empty():
             print(0)
         else:
             q1 = q.get()
             print(str(q1[1]))
     else:
-        q.put((abs(request),request))
+        q.put((abs(x), x))
