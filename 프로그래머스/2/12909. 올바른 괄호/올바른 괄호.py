42996ecba00d3af34e165b79877030a271a12a3e
채은 <88484000+Leechaeun1@users.noreply.github.com>
2025-05-10 20:13:13 +0900

    Update 올바른 괄호.py

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,26 @@
-from collections import deque
-
-d = deque()
 def solution(s):
+    stack = []
+    # 문자열을 돌면서
     for i in range(len(s)):
-
-        if s[i]=='(':
-            d.append(s[i])
-        elif s[i]==')':
-            if not d:
+        # 만약 문자열의 i번째가 '('라면
+        if s[i] == '(':
+            # 덱에 삽입
+            stack.append(s[i])
+        # 만약 문자열의 i번째가 ')'일 경우
+        elif s[i] == ')':
+            # d가 비어있다면 False
+            if not stack:
                 return False
+            # 비어있지 않다면
             else:
-                d.pop()
-    if d:
+                stack.pop()
+    # d에 남아있는 요소가 존재한다면 False
+    if stack:
         return False
     return True
+
+
+s = "()()"
+print(solution(s))
+s = ")()("
+print(solution(s))
