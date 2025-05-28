423e1d27a3e0ec79af3d551e0896f28fab668510
채은 <88484000+Leechaeun1@users.noreply.github.com>
2025-05-28 22:01:41 +0900

    [level 3] Title: 단어 변환, Time: 0.81 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 9.07 MB, 시간: 0.47 ms
+메모리: 9.27 MB, 시간: 0.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 28일 21:55:19
+2025년 05월 28일 22:01:39
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -3,22 +3,22 @@ from collections import deque
 def solution(begin, target, words):
     visited=[False]*len(words)
     
-    queue = deque([(begin,0)])
+    queue = deque([(begin, 0)])
     
     answer = 0
     
     while queue:
         current_word, steps = queue.popleft()
         
-        if current_word==target:
+        if current_word == target:
             answer = steps
             break
-        
         for i, word in enumerate(words):
             if not visited[i]:
-                diff_count=sum(1 for c1, c2 in zip(current_word, word) if c1 != c2)
+                diff_count=sum(1 for c1, c2 in zip(current_word, word) if c1!=c2)
                 if diff_count==1:
                     visited[i]=True
-                    queue.append((word, steps+1))
-    
+                    queue.append((word,steps+1))
+                    
+        
     return answer
