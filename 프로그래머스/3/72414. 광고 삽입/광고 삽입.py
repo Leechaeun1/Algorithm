ae1f23e30451c747545b6074d9bc03e058926df2
채은 <88484000+Leechaeun1@users.noreply.github.com>
2025-07-01 23:46:56 +0900

    [level 3] Title: 광고 삽입, Time: 1079.28 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -1,28 +1,3 @@
-# 광고 삽입
-def solution(p, a, l):
-    T = lambda x: int(x[:2]) * 3600 + int(x[3:5]) * 60 + int(x[6:])
-
-    R = lambda x: f'{x // 3600:02}:{x % 3600 // 60:02}:{x % 60:02}'
-
-    P, A = T(p), T(a)
-
-    t = [0] * (P + 2)
-
-    for log in l:
-        s, e = map(T, log.split('-'))
-        t[s] += 1
-        t[e] -= 1
-
-    for i in range(1, P + 1):
-        t[i] += t[i - 1]
-    for i in range(1, P + 1):
-        t[i] += t[i - 1]
-
-    m, x = t[A - 1], 0
-    
-    for i in range(A, P):
-        v = t[i] - t[i - A]
-        if v > m:
-            m, x = v, i - A + 1
-            
-    return R(x)
+def solution(play_time, adv_time, logs):
+    answer = ''
+    return answer
